0ebdbed61b103217d27e05ffc415dbb0df6bc71f
Darren McGrandle <darren@mcgrandle.com>
2024-06-22 08:54:32 +0100

    add .env

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'my-super-secret-key'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'my-not-so-secret-key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     REDIS_URL = os.environ.get('REDIS_URL') or 'redis://'
